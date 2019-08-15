830e5710d2b90787dee9b95ed664bc01fa3f158e
Willem Olding <willemolding@gmail.com>
2019-08-15 14:11:06 -0700

    version bump to 0.0.8

--- config.nix
+++ config.nix
@@ -66,7 +66,7 @@ hcp-release-hook-publish
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.7";
+   current = "0.0.8";
    # not used by version hooks in this repo
    previous = "_._._";
   };
