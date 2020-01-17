52fa56d48c09ebfac258c411e014df18f5523db1
David Meister <thedavidmeister@gmail.com>
2020-01-17 18:44:59 +0100

    Release v0.0.12

--- config.nix
+++ config.nix
@@ -60,13 +60,13 @@ hcp-release-hook-publish
   # the commit hash that the release process should target
   # this will always be behind what ends up being deployed
   # the release process needs to add some commits for changelog etc.
-  commit = "ea8b68033a4c10600ebdfdba0f56ef41e0795bf4";
+  commit = "6d45a9fdb9c1e779a1ac85ba42a261fd04b015c4";
 
   # the semver for prev and current releases
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.11";
+   current = "0.0.12";
    # not used by version hooks in this repo
    previous = "_._._";
   };
